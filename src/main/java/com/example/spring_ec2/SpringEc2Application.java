616c9d2cf22ff91ad5c5554d94bc51c30e1d6b32
sai <gudasaikumar61@gmail.com>
2024-11-30 15:20:17 +0530

    Initial commit of Spring Boot project

---
+++ src/main/java/com/example/spring_ec2/SpringEc2Application.java
@@ -0,0 +1,13 @@
+package com.example.spring_ec2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEc2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringEc2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spring_ec2/SpringEc2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_ec2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEc2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
